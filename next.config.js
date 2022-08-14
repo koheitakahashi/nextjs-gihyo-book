ccc09668b7b02e362497759298235fa219b4bb99
koheitakahashi <426koohei@gmail.com>
2022-08-14 10:56:04 +0900

    本番環境ではdata-testid 属性を削除するようにした

--- next.config.js
+++ next.config.js
@@ -2,9 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  compiler: {
-    styledComponents: true,
-  }
+  compiler: (() => {
+    // styledComponents の有効化
+    let compilerConfig = {
+      styledComponents: true,
+    }
+
+    if (process.env.NODE_ENV !== 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        // 本番環境では React Testing Library で使用する data-testid 属性を削除
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      }
+    }
+
+    return compilerConfig
+  })(),
 }
 
 module.exports = nextConfig
