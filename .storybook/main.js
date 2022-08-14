0a4223ad017b944cefaf2f6a4e91c8934f5c3f36
koheitakahashi <426koohei@gmail.com>
2022-08-14 10:26:26 +0900

    storybook の設定を変更
    Theme の適用、リセットCSS、next/image の差し替えを追加。

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const path = require('path')
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -12,5 +15,21 @@ module.exports = {
   "core": {
     "builder": "@storybook/builder-webpack5"
   },
-  "staticDirs": ['public']
+  "staticDirs": ['public'],
+  "babel": async options => ({
+    ...options,
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
+    ]
+  }),
+  "webpackFinal": async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '..', 'tsconfig.json')
+      }),
+    ]
+    return config
+  },
 }
