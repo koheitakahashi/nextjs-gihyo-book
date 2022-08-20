bad0a495deaab829f030ccf44ca13210451a91c3
koheitakahashi <426koohei@gmail.com>
2022-08-20 09:55:20 +0900

    npm run format

--- src/components/organisms/ProductForm/index.tsx
+++ src/components/organisms/ProductForm/index.tsx
@@ -1,5 +1,4 @@
 import { Controller, useForm } from 'react-hook-form'
-import type { Category, Condition } from 'types'
 import Button from 'components/atoms/Button'
 import Input from 'components/atoms/Input'
 import Text from 'components/atoms/Text'
@@ -7,6 +6,7 @@ import TextArea from 'components/atoms/TextArea'
 import Box from 'components/layout/Box'
 import Dropdown from 'components/molecules/Dropdown'
 import InputImages, { FileData } from 'components/molecules/InputImages'
+import type { Category, Condition } from 'types'
 
 export type ProductFormData = {
   image: FileData[]
